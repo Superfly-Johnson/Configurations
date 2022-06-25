eb0e410d4cb87c5e800463ca54a1ed8dc92b154b
Superfly Johnson <superfly.johnson@outlook.com>
2022-06-25 13:41:20 -0400

    [Emacs] Updated install script (forgot to tangle)

--- emacs/install.sh
+++ emacs/install.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 echo "Copying config.org to '${HOME}/.emacs.d/config.org'..."
+if [[ -d "$HOME/.emacs.d" ]]
+then
+  echo "$HOME/.emacs.d doesn't exist. Creating it..."
+  mkdir "$HOME/.emacs.d"
+fi
+cp ./init.el ${HOME}/.emacs.d/init.el
 cp ./config.org ${HOME}/.emacs.d/config.org
