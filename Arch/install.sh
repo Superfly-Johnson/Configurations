fbca71eee36906113bcc1e1ccda9daecf0f0b6c4
Superfly Johnson <superfly.johnson@outlook.com>
2022-06-26 17:30:35 -0400

    [Arch] Updated post install script, now contains Nix' configuration

---
+++ Arch/install.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+pacman -S emacs syncthing git openssh
+pacman -S nix
+systemctl enable nix-daemon.service
+echo "allowed-users = superfly" >> /etc/nix/nix.conf
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+nix-channel --update 
+nix-env --upgrade
