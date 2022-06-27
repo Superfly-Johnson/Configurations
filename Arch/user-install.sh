bee4bb671779bfa213889cf91d97c1ad2999a899
Superfly Johnson <superfly.johnson@outlook.com>
2022-06-27 16:52:40 -0400

    [Arch] Added nixpkgs-unstable to my user install script.

--- Arch/user-install.sh
+++ Arch/user-install.sh
@@ -1,4 +1,5 @@
 !/bin/sh
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home
 nix-channel --update
 echo "export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}" >> $HOME/.bashrc
