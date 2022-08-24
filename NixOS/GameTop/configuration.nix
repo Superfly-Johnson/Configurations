69eb1caa511fddc75e712956c2b97b2e89850d80
Superfly Johnson <superfly.johsnon@outlook.com>
2022-08-24 17:56:42 -0400

    [NixOS/GameTop] Added ssh-agent

--- NixOS/GameTop/configuration.nix
+++ NixOS/GameTop/configuration.nix
@@ -100,6 +100,10 @@
   #  wget
   ];
 
+  programs.ssh = {
+    startAgent = true;
+  }
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
