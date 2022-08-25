9622bd596aebeaab84680c6d3307423997b880b4
Superfly Johnson <superfly.johsnon@outlook.com>
2022-08-24 20:25:09 -0400

    [NixOS/GameTop] Added Steam

--- NixOS/GameTop/configuration.nix
+++ NixOS/GameTop/configuration.nix
@@ -99,7 +99,11 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
-
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
   programs.ssh = {
     startAgent = true;
   };
