0a3a2ec7ae301de8001bb851b9412392c63a3418
Superfly Johnson <superfly.johsnon@outlook.com>
2022-08-30 13:38:55 -0400

    [Home-Manager] Remove org-mode readme

--- Home-Manager/home.nix
+++ Home-Manager/home.nix
@@ -1,29 +1,33 @@
 {options, config, lib, pkgs, ... }:
 
 {
-  home.username = "superfly";
-  home.homeDirectory = "/home/superfly";
+    home.username = "superfly";
+    home.homeDirectory = "/home/superfly";
 
-home.stateVersion = "22.05";
+    home.stateVersion = "22.05";
 
-programs.git = {
-    enable = true;
-    userName = "Superfly Johnson";
-    userEmail = "superfly.johnson@outlook.com";
-};
+    programs.git = {
+        enable = true;
+        userName = "Superfly Johnson";
+        userEmail = "superfly.johnson@outlook.com";
+    };
 
-home.packages = [
-  pkgs.libreoffice
-];
+    home.packages = [
+        pkgs.libreoffice
+        pkgs.musescore
+        pkgs.ardour
+        pkgs.freecad
+        pkgs.keepassxc
+    ];
 
-services.syncthing.enable = true;
+    services.syncthing.enable = true;
 
-services.emacs = {
-enable = true;
-client.enable = true;
-defaultEditor = true;
-socketActivation.enable = true;
-};
+    services.emacs = {
+        enable = true;
+        client.enable = true;
+        defaultEditor = true;
+        socketActivation.enable = true;
+    };
 
-programs.home-manager.enable = true;
+    programs.home-manager.enable = true;
 }
